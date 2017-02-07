ee09b8b8a656e3b5cc8d2427e875fb3fe76d6205
Savannah <floydsm1@g.cofc.edu>
2017-02-07 09:15:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/savannah/mortgage_calculator_floyd/MortgageCalculator.java
@@ -0,0 +1,13 @@
+package com.example.savannah.mortgage_calculator_floyd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MortgageCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mortgage_calculator);
+    }
+}
